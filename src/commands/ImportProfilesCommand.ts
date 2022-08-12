0a8088b11113ce51692c04724a042d69105abe7d
samjellis <11990712+samjellis@users.noreply.github.com>
2022-08-12 01:53:33 +0100

    Remove unnecessary database login

--- src/commands/ImportProfilesCommand.ts
+++ src/commands/ImportProfilesCommand.ts
@@ -1,13 +1,8 @@
 import fs from 'fs';
-import {ConnectionOptionsReader, DataSourceOptions, DataSource} from 'typeorm';
 import Command from './Command';
 import {GuildUser} from '../entity/GuildUser';
 import extractScoreSaberID from '../util/extractScoreSaberID';
 import {SlashCommandBuilder, CommandInteraction} from 'discord.js';
-
-type Mutable<Type> = {
-    -readonly [Key in keyof Type]: Type[Key];
-};
 
 function printRow(row: {discordId: string, ssId: string}): void {
     console.log(`ScoreSaber ID: ${row.ssId}`);
@@ -21,22 +16,6 @@ export default class ImportProfilesCommand implements Command {
         .setDescription('Imports');
 
     public async execute(interaction: CommandInteraction) {
-        console.log('Connecting to database...');
-        const allDataSourceOptions = await new ConnectionOptionsReader().all();
-        if (allDataSourceOptions.length === 0) {
-            console.log('Could not find database settings.');
-            process.exit();
-        }
-        const dataSourceOptions = allDataSourceOptions[0] as Mutable<DataSourceOptions>;
-        dataSourceOptions.extra = {socketPath: '/run/mysqld/mysqld.sock'};
-        const dataSource = new DataSource(dataSourceOptions);
-        dataSource.initialize().catch((error) => {
-            console.log('Failed to connect to database');
-            console.log(error);
-            process.exit();
-        });
-        console.log('Connected to database.');
-
         const name = '../bbsdFormatted.json';
         const data = JSON.parse(fs.readFileSync(`${name}.json`).toString()) as {discordId: string, ssId: string}[];
         for (const row of data) {
